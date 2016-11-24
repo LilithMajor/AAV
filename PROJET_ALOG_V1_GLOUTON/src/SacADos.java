6ba3a16ee3a223816f48ed86e8f72452695b0164
Gustave GRAMMONT <grammont@iut.univ-paris5.fr>
2016-11-24 15:07:37 +0100

    Fini dans l'ensemble a revoir

--- PROJET_ALOG_V1_GLOUTON/src/SacADos.java
+++ PROJET_ALOG_V1_GLOUTON/src/SacADos.java
@@ -1,15 +1,29 @@
 import java.util.ArrayList;
 
 public class SacADos {
-	private ArrayList<ObjetV> objets;
+	private ArrayList<ObjetV> objets = new ArrayList<ObjetV>();
+	private double poids = 0;
 	private double poidsMax;
 	
 	public SacADos(double poidsmax){
 		this.poidsMax = poidsmax;
 	}
 	
-	public void setObjets(ArrayList<ObjetV> obj){
-		objets = obj;
+	public void ajouterObjets(ObjetV obj){
+		objets.add(obj);
+		poids = poids + obj.getPoids();
+	}
+	
+	public double getPoids(){
+		return poids;
+	}
+	
+	public double getPoidsMax(){
+		return poidsMax;
+	}
+	
+	public ArrayList<ObjetV> getObjets(){
+		return objets;
 	}
 }
 
