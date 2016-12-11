02e37a43f97374c5c062fb1fa5d267734b4f4201
roman sztergbaum <roman sztergbaum>
2016-12-11 13:26:04 +0100

    Traduction de l'algo

--- PROJET_ALOG_V1_GLOUTON/src/progDynamique/Trieur.java
+++ PROJET_ALOG_V1_GLOUTON/src/progDynamique/Trieur.java
@@ -6,6 +6,7 @@ public class Trieur {
 
 	private ArrayList<ObjetV> objets;
 	private SacADos sac;
+	private static final int nbrObjs = 4;
 	
 	public Trieur(ArrayList<ObjetV> obj, SacADos sac){
 		objets = obj;
@@ -13,12 +14,43 @@ public class Trieur {
 	}
 	
 	public ArrayList<ObjetV> tri(){
+		int maxContenance = (int) (sac.getPoidsMax());
+		double[][] matrice = new double[nbrObjs][maxContenance + 1];
+		for(int j = 0; j < maxContenance; j++){
+			if(sac.getObjets().get(0).getPoids() > j)
+				matrice[0][j] = 0;
+			else
+				matrice[0][j] = sac.getObjets().get(0).getValeur();
+		}
+		for(int i = 1; i < maxContenance; i++){
+			for(int j = 0; j < maxContenance; j++){
+				if(sac.getObjets().get(i).getPoids() > j)
+					matrice[i][j] = matrice[i-1][j];
+				else
+					matrice[i][j] = Math.max(matrice[i-1][j], matrice[i-1][(int) (j-sac.getObjets().get(i).getPoids())] + sac.getObjets().get(i).getValeur());
+			}
+		}
+		int i = nbrObjs;
+		int j = maxContenance;
+		while(matrice[i][j] == matrice[i][j - 1]){
+			j--;
+		}
+		while(j>0){
+			while(i > 0 && matrice[i][j] == matrice[i - 1][j]){
+				i--;
+			}
+			j = (int) (j - sac.getObjets().get(i).getPoids());
+			if(j > 0)
+				objets.add(sac.getObjets().get(i));
+			i--;
+		}
+//Bon en gros j'ai traduit l'algo d'OC en java inchallah ça marche du premier coup
 //		Collections.sort(objets);
 		return objets;
 	}
 	
+
 	public void remplissage(){
-		int[lol;
 //		for(ObjetV o : objets){
 //			if (sac.getPoids() + o.getPoids() <= sac.getPoidsMax()){
 //				sac.ajouterObjets(o);
